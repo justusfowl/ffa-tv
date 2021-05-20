29f0303200cbd5d21766cf3bdc8fbcd0cbd7449a
JustusFowl <justusfowl@gmail.com>
2021-05-21 01:23:40 +0200

    upgrade angular

---
+++ frontend/upgrade.ps1
@@ -0,0 +1,5 @@
+ng update @angular/core@9 @angular/cli@9 --create-commits
+ng update @angular/core@10 @angular/cli@10 --create-commits
+ng update @angular/core@11 @angular/cli@11 --create-commits
+ng update @angular/core@12 @angular/cli@12 --create-commits
+ng update @angular/core@12 @angular/cli@12 --create-commits
