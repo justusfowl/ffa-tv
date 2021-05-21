846d76286153a41687ec7e318b97576e92fb964b
JustusFowl <justusfowl@gmail.com>
2021-05-21 08:35:50 +0200

    @angular/core migration - migration-v11-wait-for-async
    `async` to `waitForAsync` migration. The `async` testing function has been renamed to `waitForAsync` to avoid confusion with the native `async` keyword.

--- frontend/src/app/pages/rssfeed/rssfeed.component.spec.ts
+++ frontend/src/app/pages/rssfeed/rssfeed.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { RssfeedComponent } from './rssfeed.component';
 
@@ -6,7 +6,7 @@ describe('RssfeedComponent', () => {
   let component: RssfeedComponent;
   let fixture: ComponentFixture<RssfeedComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ RssfeedComponent ]
     })
