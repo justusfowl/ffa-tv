4a8923a2e04f52959fba089b7f1936cde1bae5c7
JustusFowl <justusfowl@gmail.com>
2021-05-21 08:10:53 +0200

    @angular/core migration - migration-v9-missing-injectable
    Missing @Injectable and incomplete provider definition migration. In Angular 9, enforcement of @Injectable decorators for DI is a bit stricter and incomplete provider definitions behave differently. Read more about this here: https://v9.angular.io/guide/migration-injectable

--- frontend/src/app/custome.route.strategy.ts
+++ frontend/src/app/custome.route.strategy.ts
@@ -1,5 +1,7 @@
 import { ActivatedRouteSnapshot, RouteReuseStrategy, DetachedRouteHandle } from '@angular/router';
+import { Injectable } from "@angular/core";
 
+@Injectable()
 export class CustomReuseStrategy implements RouteReuseStrategy {
 
     shouldDetach(route: ActivatedRouteSnapshot): boolean {
