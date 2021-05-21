9ba548619a1f6273fa8e4a5d443c625a06e11b95
JustusFowl <justusfowl@gmail.com>
2021-05-21 08:25:16 +0200

    upgrade with force

--- frontend/upgrade.ps1
+++ frontend/upgrade.ps1
@@ -1,8 +1,8 @@
-ng update @angular/core@9 @angular/cli@9 --create-commits
-ng update @angular/material@9 --create-commits
-ng update @angular/core@10 @angular/cli@10 --create-commits
-ng update @angular/material@10 --create-commits
-ng update @angular/core@11 @angular/cli@11 --create-commits
-ng update @angular/material@11 --create-commits
-ng update @angular/core@12 @angular/cli@12 --create-commits
-ng update @angular/material@12 --create-commits
+ng update @angular/core@9 @angular/cli@9 --create-commits --force
+ng update @angular/material@9 --create-commits --force
+ng update @angular/core@10 @angular/cli@10 --create-commits --force
+ng update @angular/material@10 --create-commits --force
+ng update @angular/core@11 @angular/cli@11 --create-commits --force
+ng update @angular/material@11 --create-commits --force
+ng update @angular/core@12 @angular/cli@12 --create-commits --force
+ng update @angular/material@12 --create-commits --force
