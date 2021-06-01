2e04e2381169fdd80da1f1b6cef699b495c0219d
JustusFowl <justusfowl@gmail.com>
2021-06-01 07:59:36 +0200

    Avoiding load error of serving static files and conflicting angular routing

--- server.js
+++ server.js
@@ -7,6 +7,15 @@ const server = http.Server(app);
 
 app.use('/', express.static('frontend/dist/ffa-tv'));
 
+const buildLocation = 'frontend/dist/ffa-tv';
+app.use((req, res, next) => {
+  if (!req.originalUrl.includes(buildLocation)) {
+    res.sendFile(`${__dirname}/${buildLocation}/index.html`);
+  } else {
+    next();
+  }
+});
+
 server.listen(port, () => {
     console.info('Server started at port :' + port);
   });
